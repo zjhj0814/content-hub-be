580c3be74430896dde5d6403fddea3fc08481175
zjhj0814 <zjhj0814@naver.com>
2024-11-13 18:04:09 +0900

    spring boot initializer

---
+++ src/main/java/tibetyo/content_hub/ContentHubApplication.java
@@ -0,0 +1,13 @@
+package tibetyo.content_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tibetyo/content_hub/ContentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package tibetyo.content_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
