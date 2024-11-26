6a6bf544a0c119b943dda009e35be99474fe9746
zjhj0814 <zjhj0814@naver.com>
2024-11-26 15:52:55 +0900

    fix: WebDriverUtil은 웹 드라이버 초기화 및 종료를 관리한다

--- src/main/java/tibetyo/content_hub/WebDriverUtil.java
+++ src/main/java/tibetyo/content_hub/WebDriverUtil.java
@@ -1,41 +1,27 @@
 package tibetyo.content_hub;
 
-import static tibetyo.content_hub.WebDriverProperty.*;
-
-import java.util.List;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static tibetyo.content_hub.WebDriverProperty.WEB_DRIVER_ID;
+import static tibetyo.content_hub.WebDriverProperty.WEB_DRIVER_PATH;
 
 public class WebDriverUtil {
     private WebDriver driver;
-    public WebDriverUtil(){
-        process();
-    }
 
-    private void process(){
-        System.setProperty(WEB_DRIVER_ID.getPath(), WEB_DRIVER_PATH.getPath());
-        driver = new ChromeDriver();
-
-        try{
-            getDramaData();
-        } catch(InterruptedException e){
-            e.printStackTrace();
+    public WebDriver getDriver() {
+        if (driver == null) {
+            System.setProperty(WEB_DRIVER_ID.getPath(), WEB_DRIVER_PATH.getPath());
+            driver = new ChromeDriver();
         }
-
-        driver.close();
-        driver.quit();
+        return driver;
     }
 
-    private void getDramaData() throws InterruptedException{
-        driver.get("https://www.wavve.com/?utm_source=GA&utm_medium=SA&utm_campaign=WAVVE_GA_NU_PC&utm_content=0.%EB%B8%8C%EB%9E%9C%EB%93%9C%ED%82%A4%EC%9B%8C%EB%93%9C_%ED%99%95%EC%9E%A5%EA%B2%80%EC%83%89&utm_term=WAVVE&gad_source=1&gclid=CjwKCAiAudG5BhAREiwAWMlSjBgVd4byl9qxvSNmzzhXzEyejcgqLWSu8TJ647OcntP8RVRev9c6hBoCfvwQAvD_BwE");
-
-        WebElement dramaSelectionLink = driver.findElement(By.xpath("//a[contains(text(), '드라마')]"));
-        dramaSelectionLink.click();
-        Thread.sleep(3000); //webdriverwait 사용으로 변경
-
-
+    private void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 
 }
