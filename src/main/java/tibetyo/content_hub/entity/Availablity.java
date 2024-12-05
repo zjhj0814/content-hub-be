2874615b52d0d25457aba8e79239e50c54ad29b6
zjhj0814 <zjhj0814@naver.com>
2024-12-05 21:23:15 +0900

    feat: OTT별 Content 가능 여부를 저장하는 Availablity 엔티티 생성

---
+++ src/main/java/tibetyo/content_hub/entity/Availablity.java
@@ -0,0 +1,33 @@
+package tibetyo.content_hub.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Availablity {
+    @Id
+    @GeneratedValue
+    @Column(name = "availablity_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id")
+    private Content content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ott_id")
+    private Ott ott;
+
+    @Enumerated(EnumType.STRING)
+    private ContentStatus contentStatus;
+
+    @Builder
+    public Availablity(Long id, Content content, Ott ott, ContentStatus contentStatus) {
+        this.id = id;
+        this.content = content;
+        this.ott = ott;
+        this.contentStatus = contentStatus;
+    }
+}
