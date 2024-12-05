c08bba9563df67f60e92caa16863b884e12c2017
zjhj0814 <zjhj0814@naver.com>
2024-12-05 21:27:30 +0900

    feat: ContentStatus 생성
    - 무료제공, 구독자 제공, 개별구매, 제공되지 않음 상태를 가진다

---
+++ src/main/java/tibetyo/content_hub/entity/ContentStatus.java
@@ -0,0 +1,11 @@
+package tibetyo.content_hub.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum ContentStatus {
+    FREE,
+    SUBSCRIBER_PROVISION,
+    INDIVIDUAL_PURCHASE,
+    NOT_PROVIDED
+}
