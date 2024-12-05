133e332822b55b88aa3cee495d33184ba5d8e478
zjhj0814 <zjhj0814@naver.com>
2024-12-05 17:16:32 +0900

    fix: 길이가 긴 description 관련 보수
    - 길이가 긴 String의 경우 DB insert에 에러가 발생하므로 개발중 mysql TEXT 컬럼이 생성되도록 엔티티 어노테이션을 작성한다

--- src/main/java/tibetyo/content_hub/content/Content.java
+++ src/main/java/tibetyo/content_hub/content/Content.java
@@ -1,20 +1,13 @@
 package tibetyo.content_hub.content;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import tibetyo.content_hub.ContentCast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -24,6 +17,8 @@ public class Content {
     @Column(name = "content_id")
     private Long id;
     private String title;
+    @Lob
+    @Column(columnDefinition = "TEXT")
     private String description;
     @Enumerated(EnumType.STRING)
     private ContentCategory category;
