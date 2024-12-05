2d15570ad7bcd862f675ddfd1b7294dc5299adcd
zjhj0814 <zjhj0814@naver.com>
2024-12-05 22:10:57 +0900

    feat: user entity 생성

---
+++ src/main/java/tibetyo/content_hub/entity/User.java
@@ -0,0 +1,31 @@
+package tibetyo.content_hub.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(unique = true, nullable = false, name = "user_id")
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String email, String password, Long id) {
+        this.email = email;
+        this.password = password;
+        this.id = id;
+    }
+}
